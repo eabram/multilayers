20883e616e75401a44e7e98a14f8d8a503ebea24
abram <abram@arcnl.nl>
2023-06-16 19:00:42 +0200

    Update

---
+++ multilayers/__init__.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os,sys
+import scipy.integrate
+import pandas as pd
+import scipy.interpolate
+
+from .multilayers import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='multilayers',
+      version='1.0.1',
+      description='Multilayer calculation with the Tranfer Matrix Method also including absoption profiles',
+      url='https://git.amolf.nl/abram/multilayers',
+      author='Ester Abram',
+      author_email='E.abram@arcnl.nl',
+      license='ARCNL',
+      packages=['multilayers'],
+      zip_safe=False)
